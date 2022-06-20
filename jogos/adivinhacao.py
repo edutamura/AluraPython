0929e0df9adc6a43c7419c2d707966ae3bdcce55
Eduardo <edutamura@gmail.com>
2022-06-20 15:50:42 -0300

    adicionado break e continue para quando o usuário acerta antes do fim das tentativas e quando o usuário digita um valor que não esteja entre 1 e 100

--- jogos/adivinhacao.py
+++ jogos/adivinhacao.py
@@ -9,9 +9,13 @@ rodada = 1
 for rodada in range(1, total_de_tentativas + 1):
 
     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
-    chute_str = input("Digite o seu número: ")
+    chute_str = input("Digite o seu número entre 1 e 100: ")
     print("Você digitou ", chute_str)
     chute = int(chute_str)
+
+    if(chute < 1 or chute > 100):
+        print("Você deve digitar um número entre 1 e 100!")
+        continue
 
     acertou = chute == numero_secreto
     maior = chute > numero_secreto
@@ -19,6 +23,7 @@ for rodada in range(1, total_de_tentativas + 1):
 
     if (acertou):
         print("Você acertou!")
+        break
     else:
         if (maior):
             print("Você errou! O seu chute foi maior que o número secreto.")
