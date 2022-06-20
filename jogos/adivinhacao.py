8d77939b13df4f48afd25815a0f47848bfe9546f
Eduardo <edutamura@gmail.com>
2022-06-20 15:45:41 -0300

    troca do while por for

--- jogos/adivinhacao.py
+++ jogos/adivinhacao.py
@@ -6,8 +6,9 @@ numero_secreto = 42
 total_de_tentativas = 3
 rodada = 1
 
-while(rodada <= total_de_tentativas):
-    print("Tentativas: ", rodada, "de", total_de_tentativas)
+for rodada in range(1, total_de_tentativas + 1):
+
+    print("Tentativa {} de {}".format(rodada, total_de_tentativas))
     chute_str = input("Digite o seu número: ")
     print("Você digitou ", chute_str)
     chute = int(chute_str)
@@ -23,9 +24,5 @@ while(rodada <= total_de_tentativas):
             print("Você errou! O seu chute foi maior que o número secreto.")
         elif (menor):
             print("Você errou! O seu chute foi menor que o número secreto.")
-    
-    rodada = rodada + 1
-
-    #teste de sincronização
 
 print("Fim do jogo")
