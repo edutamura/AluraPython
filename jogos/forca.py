9c4e2e26a2209f73f1a365d9dbed5a51806300ae
Eduardo <edutamura@gmail.com>
2022-06-26 14:42:33 -0300

    Python 3 parte 2 - Aula 05 - criação de limite de erros e implementado list comprehension para iniciar a lista letras_acertadas

--- jogos/forca.py
+++ jogos/forca.py
@@ -4,11 +4,12 @@ def jogar():
     print("***Bem vindo ao jogo de forca!***")
     print("*********************************")
 
-    palavra_secreta = "banana"
-    letras_acertadas = ["_", "_", "_", "_", "_", "_"]
+    palavra_secreta = "banana".upper()
+    letras_acertadas = ["_" for letra in palavra_secreta]
 
     enforcou = False
     acertou = False
+    erros = 0
 
     print(letras_acertadas)
 
@@ -17,15 +18,26 @@ def jogar():
     while(not enforcou and not acertou):
         
         chute = input("Qual letra? ")
-        chute = chute.strip()
+        chute = chute.strip().upper()
 
-        index = 0
-        for letra in  palavra_secreta:
-            if(chute.upper() == letra.upper()):
-                letras_acertadas[index] = letra
-            index = index + 1
+        if(chute in palavra_secreta):
+            index = 0
+            for letra in  palavra_secreta:
+                if(chute == letra):
+                    letras_acertadas[index] = letra
+                index += 1
 
-        print(letras_acertadas)
+            print(letras_acertadas)
+        else:
+            erros += 1
+
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas
+
+    if(acertou):
+        print("Você ganhou!")
+    else:
+        print("Você perdeu!")
 
     print("Fim do jogo")
 
