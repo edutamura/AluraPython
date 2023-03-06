fa6acd1ecfebf5901bc39b7a807e9621c1d1740b
edutamura <51760396+edutamura@users.noreply.github.com>
2023-03-06 15:52:46 -0300

    Ajuste função marca_chute_correto
    Foi removido um espaço a mais que havia no for da função marca_chute_correto.

--- jogos/forca.py
+++ jogos/forca.py
@@ -68,7 +68,7 @@ def pede_chute():
 
 def marca_chute_correto(chute, letras_acertadas, palavra_secreta):
     index = 0
-    for letra in  palavra_secreta:
+    for letra in palavra_secreta:
         if(chute == letra):
             letras_acertadas[index] = letra
         index += 1
